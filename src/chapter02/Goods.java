f32f9c9d44656f62e3cf9a0e3f2df3f6d1a3f58f
BIT <BIT@DESKTOP-E23PFRN>
2018-12-27 16:11:22 +0900

    Start Chapter02

---
+++ src/chapter02/Goods.java
@@ -0,0 +1,42 @@
+package chapter02;
+
+public class Goods {
+	
+	private String name;
+	private int price;
+	private int countStock;
+	private int countSold;
+	//인스턴스 변수 - heap영역 안에서 하나당 4btye를 할당 받는다.
+	//private로 하면 다른 class에서 사용할수 없다. 하지만 javabean을 통해서 사용할 수 있다.
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		if(price<0) {
+			price=0;
+		}
+		this.price = price;
+	}
+	public int getCountStock() {
+		return countStock;
+	}
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+	public int getCountSold() {
+		return countSold;
+	}
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+	
+	
+	
+}
---
+++ src/chapter02/GoodsApp.java
@@ -0,0 +1,23 @@
+package chapter02;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		
+		Goods goods = new Goods();
+		//지역변수 - main이라는 block안에 있기때문 / 지역변수는 stack에 안들어진다.
+
+		//goods.name = "hello"; //private로 이루어져 있기때문에 사용할 수 없다.
+		goods.setName("nikon");
+		goods.setPrice(400000);
+		goods.setCountStock(30);
+		goods.setCountSold(50);
+		
+		System.out.println("상품이름: "+goods.getName()+
+							",가격: "+goods.getPrice()+
+							",재고개수: "+goods.getCountStock()+
+							",팔린개수: "+goods.getCountSold());
+		
+	}
+
+}
