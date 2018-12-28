6e0be324a04835afc2ec7cedab81f2887116e859
BIT <BIT@DESKTOP-E23PFRN>
2018-12-28 11:15:37 +0900

    181228 11:15 Class 3 Change

--- src/chapter02/SwapTest.java
+++ src/chapter02/SwapTest.java
@@ -4,6 +4,9 @@ public class SwapTest {
 
 	public static void main(String[] args) {
 
+		//
+		//call by value
+		//
 		int a = 10;
 		int b = 20;
 		System.out.println(a+" : "+b);
@@ -18,7 +21,21 @@ public class SwapTest {
 		
 		
 		System.out.println(a+" : "+b);
+		
+		//
+		//call by reference ( 객체로 만들어서 하는것 )
+		//
+		Value m = new Value();
+		m.value = 10;
+		Value n = new Value();
+		n.value = 20;
 
+		System.out.println(m.value+" : "+n.value);
+		
+		swap(m,n);
+		
+		System.out.println(m.value+" : "+n.value);
+				
 	}
 	
 	public static void swap(int i, int j) {
@@ -27,5 +44,11 @@ public class SwapTest {
 		j=temp;
 		//함수호출이 끝나면 i,j,temp의 변수가 stack에서 빠져나온다.
 	}
+	public static void swap(Value i, Value j) {
+		int temp = i.value;
+		i.value=j.value;
+		j.value=temp;
+	}
+	//같이 이름의 함수가 있는데 안에 있는 변수가 다르거나, return 값의 유무에 따라 다른것이 : 오버로딩이라고 한다.
 
 }
