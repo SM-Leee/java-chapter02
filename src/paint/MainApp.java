0a1eae8a97ff7baee9d8d1ee86f2a01bc29c540e
BIT <BIT@DESKTOP-E23PFRN>
2018-12-28 13:54:34 +0900

    181228 Class 4 Change

---
+++ src/paint/MainApp.java
@@ -0,0 +1,14 @@
+package paint;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Point point = new Point();
+		point.setX(100);
+		point.setY(200);
+		
+		point.show();
+
+	}
+
+}
