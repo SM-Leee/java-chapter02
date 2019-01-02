3fad24e852f5465f6e9c82327dcc01a28633292d
BIT <BIT@DESKTOP-E23PFRN>
2019-01-02 10:51:01 +0900

    190102 10:50 Shape Class 추가 및 상속(오버라이딩 / 추상클래스)

--- src/paint/MainApp.java
+++ src/paint/MainApp.java
@@ -6,24 +6,47 @@ public class MainApp {
 		Point point = new Point();
 		point.setX(100);
 		point.setY(200);
+		drawPoint(point);
 		
-		point.show();
+		//point.show();
 		
 		Point point2 = new Point(50, 50);
-		point2.show();
-		point2.show(false);
+		drawPoint(point2);
+		//point2.show();
+		//point2.show(false);
 		
 		Point p3 = new ColorPoint();
 		p3.setX(100);
 		p3.setY(50);
 		((ColorPoint)p3).setColor("Red");
-		p3.show();
-		p3.show(false);
+		//p3.show();
+		//p3.show(false);
+		drawPoint(p3);
+		
+		Shape triangle = new Triangle();
+		drawShape(triangle);
+		
+		Shape rect = new Rect();
+		drawShape(rect);
+		
+		Shape circle = new Circle();
+		drawShape(circle);
 
 	}
 	
 	public static void drawPoint(Point point) {
 		point.show();
 	}
+	
+	public static void drawShape(Shape shape) {
+		shape.draw();
+	}
+	
+	/*
+	 * public static void drawTriangle(Triangle triangle) { triangle.draw(); }
+	 * public static void drawRect(Rect rect) { rect.draw(); }
+	 */
+	
+	
 	
 }
